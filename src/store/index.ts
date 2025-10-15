dc5820d8590001877565eafab657b503d216eca2
Miguel Machado <hello@miguelmachado.dev>
2025-10-15 20:07:04 -0300

    refactor: replace todo app with video player interface and add Tailwind CSS

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,26 +1,9 @@
-import { configureStore, createSlice } from "@reduxjs/toolkit";
+import { configureStore } from "@reduxjs/toolkit";
 import { useSelector, type TypedUseSelectorHook } from "react-redux";
 
-// Com base no name ('todo') e nas chaves do reducers ('add'),
-// ele cria o tipo da ação, como 'todo/add'. Você não precisa mais escrever isso.
-const todoSlice = createSlice({
-  name: "todo",
-  initialState: ["Fazer café"],
-
-  reducers: {
-    add: (state, action) => {
-      state.push(action.payload.newTodo);
-    },
-  },
-});
-
 export const store = configureStore({
-  reducer: {
-    todo: todoSlice.reducer,
-  },
+  reducer: {},
 });
-
-export const { add } = todoSlice.actions;
 
 export type RootState = ReturnType<typeof store.getState>;
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
